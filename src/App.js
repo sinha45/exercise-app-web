9436c53bdbc8396aa2a736d6c47d761fdf2f90cc
sinha45 <alfa45leonidas@gmail.com>
2022-11-12 01:47:49 +0600

    header added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import Card from './components/Card/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='ml-44 mt-32 float-left'>
+            <h1 className='text-3xl font-bold text-cyan-400'>ULTRA-STUDENT-CLUB</h1>
+            <h3 className='text-3xl font-semibold mt-7'>Select today's exercise</h3>
+        </div>
+    );
+};
+
+export default Header;
